fc9eb698f5f578f8a89ff7c24934bd31213b1b8e
Sheila Kagawa <145503159+shlqjn@users.noreply.github.com>
2025-08-05 04:11:26 -0700

    Updated with correct links for JS and CSS

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ themeButton.addEventListener("click", changeTheme);
 //corgi fun facts
 function showFact() {
   let facts = [
-    "Corgis were originally bred to herd cattle—imagine those tiny legs moving cows!",
+    "Corgis were originally bred to herd cattle imagine those tiny legs moving cows!",
     "Queen Elizabeth II had more than 30 corgis in her lifetime.",
-    "Corgis have double-layered coats and are *professional* shedders.",
+    "Corgis have double layered coats and are *professional* shedders.",
     "Despite their stubby legs, corgis are super fast and love to zoom!",
-    "Their ears are like radar dishes—one bark and every squirrel in the zip code knows.",
+    "Their ears are like radar dishes one bark and every squirrel in the zip code knows.",
   ];
 
   let randomNumber = Math.floor(Math.random() * facts.length);
